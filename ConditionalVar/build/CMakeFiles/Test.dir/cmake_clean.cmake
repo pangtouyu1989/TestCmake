3a5ecab8707aba7a72c4e84fa8a9941bf20ba929
sunjie <869597040@qq.com>
2021-07-16 10:36:50 +0800

    Cmake测试

---
+++ ConditionalVar/build/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/Release/Test_r"
+  "../bin/Release/Test_r.pdb"
+  "CMakeFiles/Test.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
